6f310392bb3ffa29e6db053112b71f806d1e8219
dimchee <dimchee90@gmail.com>
2022-06-24 23:58:20 +0200

    Fix Install script

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 download() {
-    echo "Downloading binary: " $1
+    echo "Downloading version: " $1
     curl -fsSL https://github.com/dimchee/prochrome/releases/download/$1/prochrome --output prochrome
     mkdir -p target/release/
-    mv -f prochrome target/release/
-    chmod a+x target/release/prochrome
+    mv -f prochrome target/debug/
+    chmod a+x target/debug/prochrome
     echo "All done :D"
 }
 
 if [[ $(uname) != "Linux" ]]; then
     echo "Sorry, looks you are not running Linux, try compiling yourself"
 else
-    download $1
+    download v0.0.2
 fi
