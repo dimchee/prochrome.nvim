a351758cb3cd3a295e2efc4b82d0bd503705ffd7
dimchee <dimchee90@gmail.com>
2022-09-25 23:44:00 +0200

    Fix install.sh to use 'latest' release

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
-download() {
-    echo "Downloading version: " $1
-    curl -fsSL https://github.com/dimchee/prochrome/releases/download/$1/prochrome --output prochrome
+if [[ $(uname) != "Linux" ]]; then
+    echo "Sorry, looks you are not running Linux, try compiling yourself"
+else
+    echo "Downloading latest binary"
+    curl -fsSL https://github.com/dimchee/prochrome/releases/download/latest/prochrome --output prochrome
     mkdir -p target/debug/
     mv -f prochrome target/debug/
     chmod a+x target/debug/prochrome
     echo "All done :D"
-}
-
-if [[ $(uname) != "Linux" ]]; then
-    echo "Sorry, looks you are not running Linux, try compiling yourself"
-else
-    download v0.0.2
 fi
